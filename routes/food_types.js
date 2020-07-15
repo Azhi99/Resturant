87a3d58415933dbb0959ee19920c8cb8da0dee37
Tishko serwan <tishko.serwan@gmail.com>
2020-07-15 13:33:16 +0300

    commit

--- routes/food_types.js
+++ routes/food_types.js
@@ -2,45 +2,63 @@ const express = require("express");
 const router = express.Router();
 
 const { db } = require("../DB/db_config.js");
-const { createValidation, deleteValidation, updateValidation } = require("../validators/food_types.js");
+const {
+  createValidation,
+  deleteValidation,
+  updateValidation,
+} = require("../validators/food_types.js");
 
 router.post("/addFoodType", createValidation, (req, res) => {
-    db("tbl_food_types").insert({
-        type_name: req.body.type_name
-    }).then((data)=>{
-        return res.json({
-            message: "1 Type Added",
-            data
-        });
-    }).catch((err)=>{
-        if(err.errno === 1062){
-            return res.status(500).json({
-                message: "This type is already exist"
-            });
-        }
+  db("tbl_food_types")
+    .insert({
+      type_name: req.body.type_name,
+    })
+    .then((data) => {
+      return res.json({
+        message: "1 Type Added",
+        data,
+      });
+    })
+    .catch((err) => {
+      if (err.errno === 1062) {
         return res.status(500).json({
-            message: err
+          message: "This type is already exist",
         });
+      }
+      return res.status(500).json({
+        message: err,
+      });
     });
 });
 
 // data is the number of updates rows
-router.patch("/updateFoodType/:id", updateValidation, createValidation, (req,res) => {
-    db("tbl_food_types").where("type_id", req.params.id).update({
-        type_name: req.body.type_name
-    }).then((data) => {
+router.patch(
+  "/updateFoodType/:id",
+  updateValidation,
+  createValidation,
+  (req, res) => {
+    db("tbl_food_types")
+      .where("type_id", req.params.id)
+      .update({
+        type_name: req.body.type_name,
+      })
+      .then((data) => {
         return res.json({
-            message: data+ " Type Updated"
+          message: data + " Type Updated",
         });
-    });
-});
+      });
+  }
+);
 
 // data is the number of deleted rows
-router.delete("/deleteFoodType/:id", deleteValidation, (req,res) => {
-    db("tbl_food_types").where("type_id", req.params.id).del().then((data)=>{ 
-        return res.json({
-            message: data + " Type Deleted",
-        });
+router.delete("/deleteFoodType/:id", deleteValidation, (req, res) => {
+  db("tbl_food_types")
+    .where("type_id", req.params.id)
+    .del()
+    .then((data) => {
+      return res.json({
+        message: data + " Type Deleted",
+      });
     });
 });
 
