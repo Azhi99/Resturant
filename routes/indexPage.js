8ee3d55e17d440f76c98205bdc43fe37e8b30de0
Azhi99 <azhijabar.aj@gmail.com>
2020-07-20 13:59:55 -0500

    commit

--- routes/indexPage.js
+++ routes/indexPage.js
@@ -6,7 +6,6 @@ router.post("/getData", async (req,res) => {
     const [{daily_sold}] = await db("tbl_invoice").sum("amount_paid as daily_sold").limit(1);
     const [{no_of_users}] = await db("tbl_users").count("* as no_of_users").limit(1);
     const [{no_of_foods}] = await db("tbl_foods").count("* as no_of_foods").limit(1);
-    const [{no_of_tables}] = await db("tbl_tables").count("* as no_of_tables").limit(1);
     const food_types = await db("tbl_food_types").select();
     const tables = await db("tbl_tables").select();
     const [dollar_price] = await db("tbl_dollar").select(["dinar", "last_update"]).where("dollar_id", 1).limit(1);
@@ -14,7 +13,6 @@ router.post("/getData", async (req,res) => {
         daily_sold,
         no_of_users,
         no_of_foods,
-        no_of_tables,
         food_types,
         tables, 
         dollar_price
