8e146f0b00d0c44300a08bbc0bcfc5453459ea45
Azhi99 <azhijabar.aj@gmail.com>
2020-07-21 12:01:39 -0500

    commit

--- app.js
+++ app.js
@@ -20,11 +20,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-// app.use(express.static("public"));
-
-app.get('/', (req, res) => {
-  res.send('hello')
-})
+app.use(express.static("public"));
 
 app.use("/user", userRouter);
 app.use("/food_type", foodTypesRouter);
